1cc46a4c917421b7b41420b86e97764e5fb2c162
suprohub <suprohub@gmail.com>
2023-07-24 13:58:41 +0300

    Добавил конвертеры и другое

--- src/objects.rs
+++ src/objects.rs
@@ -8,7 +8,19 @@ struct Triangle {
 }
 
 impl Triangle {
-    fn new() -> Triangle {
+    pub fn new() -> Triangle {
         Triangle {color: Color(255, 245, 194), p1: Vec4D::new(), p2: Vec4D::new(), p3: Vec4D::new()}
+    }
+    pub fn position(self) -> Vec3D {
+        (self.p1 + self.p2 + self.p3).convert_3d() / 3
+    }
+    pub fn norm(self) -> Vec3D {
+        // Implement
+    }
+    pub fn distance(self, vector: &Vec3D) -> f64 {
+        self.norm().dot(p1.convert_3d() - vector)
+    }
+    pub fn calculate_normal(self) -> Vec3D {
+        // неуспел сделать
     }
 }
